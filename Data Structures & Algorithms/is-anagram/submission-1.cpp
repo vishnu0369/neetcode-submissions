d7c7354bc9c4c1d4afa01241f09f3cdd64dfc8e2
vishnu0369 <88284126+vishnu0369@users.noreply.github.com>
2026-06-20 23:13:36 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) return false;
+        unordered_map<char,int>m;
+        for(auto i : s) 
+            m[i]++;
+
+        for(auto i : t) {
+            if(--m[i] < 0)
+                return false;
+        }
+        return true;
+    }
+};
