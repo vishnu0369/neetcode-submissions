10f90bd61f44364af754f46fa24bb7190b8f6c86
vishnu0369 <88284126+vishnu0369@users.noreply.github.com>
2026-07-20 08:46:11 +0530

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        int n = board.size();
+        vector<set<int>> row(n), col(n), box(n);
+
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<n;j++) {
+                if(board[i][j] != '.'){
+                    if(row[i].find(board[i][j]) != row[i].end())
+                        return false;
+                    else 
+                        row[i].insert(board[i][j]);
+
+                    if(col[j].find(board[i][j]) != col[j].end())
+                        return false;
+                    else
+                        col[j].insert(board[i][j]);
+
+                    int x = (i/3)*3 + j/3;
+                    if(box[x].find(board[i][j]) != box[x].end())
+                        return false;
+                    else
+                        box[x].insert(board[i][j]);
+                }
+            }
+        }
+        return true;
+    }
+};
