dfd85dbca3e40f489e36e336aad81c7ee2c191cf
vishnu0369 <88284126+vishnu0369@users.noreply.github.com>
2026-08-14 22:33:07 +0530

    Add: longest-repeating-substring-with-replacement - submission-6

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-6.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        unordered_map<int,int>m;
+        int x = k, l = 0, ans = 0, mx = 0, n = s.size();
+        char c = s[0];
+        for(int i=0;i<n;i++) {
+            m[s[i]]++;
+            mx=max(mx, m[s[i]]);
+            if((i-l+1) - mx > k) {
+                while((i-l+1)-mx > k) {
+                    m[s[l]]--;
+                    l++;
+                }
+            }
+            ans = max(ans, (i-l+1));
+        }
+        return ans;
+    }
+};
