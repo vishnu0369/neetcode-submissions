580d2539b75638d8fe4d36b0c8692317fc1c7d8a
vishnu0369 <88284126+vishnu0369@users.noreply.github.com>
2026-08-16 17:29:46 +0530

    Add: minimum-window-with-characters - submission-5

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-5.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        int n = s.size(), m = t.size(),l = 0,ans = INT_MAX;
+        unordered_map<char,int>need, window;
+        string res = "";
+
+        for(int i=0;i<m;i++) 
+            need[t[i]]++;
+        
+        int have = 0, uniq = need.size();
+        for(int i=0;i<n;i++) {
+            window[s[i]]++;
+            if(need[s[i]] == window[s[i]]){
+                have++;
+            }   
+
+            if(have == uniq) {
+                while(l<=i && have == uniq) {
+                    if(need.find(s[i]) != need.end() && need[s[l]] == window[s[l]]) {
+                        if((i-l+1) < ans) {
+                            ans = (i-l+1);
+                            res = s.substr(l,ans);
+                        }
+                        break;
+                    }
+                    window[s[l]]--;
+                    l++;
+                }
+            }
+        }
+        return res;
+    }
+};
